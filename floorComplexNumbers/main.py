4944b51e8f262a0fbb21e45e6f03d77c12e22bfa
vidur2 <vmod2005@gmail.com>
2022-06-29 11:56:32 -0400

    thing

--- floorComplexNumbers/main.py
+++ floorComplexNumbers/main.py
@@ -1,34 +1,50 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-def transformation_floor_b(b: int, c: int, d: float):
-    firstTerm = np.float(b/2)
+def transformation_floor_b(b: float, c: float, d: float, f: float):
+    firstTermReal = -d + b/2
 
-    secondTerm = ((((b ** 2 - 4 * c + 4 * b * d) ** 2 + 16*b**2) ** np.float(0.5) + b ** 2 - 4*c + 4 * b * d)/2) ** np.float(0.5)
+    firstTermImaginary = -f
 
-    thirdTerm = ((((b ** 2 - 4 * c + 4 * b * d) ** 2 + 16*b**2) ** np.float(0.5) - b**2 +4*c - 4 * b * d)/2) ** np.float(0.5)
+    # print("Modulus: " + str(((2 * d**2 + b * d + (b**2)/4 - 2 * f ** 2 - c) ** 2 + (4 * d * f + b * f) ** 2 ) ** 0.5))
+    # print(2 * d ** 2 + b * d +( b ** 2) / 4 - 2 * f ** 2 - c)
 
-    return (firstTerm + secondTerm, firstTerm - secondTerm, thirdTerm)
+    secondTermReal = round((((((2 * d**2 + b * d + (b**2)/4 - 2 * f ** 2 - c) ** 2 + (4 * d * f + b * f) ** 2 ) ** 0.5) + 2 * d ** 2 + b * d +( b ** 2) / 4 - 2 * f ** 2 - c)/2), 6) ** 0.5
+
+    secondTermImaginary = round((((((2 * d**2 + b * d + (b**2)/4 - 2 * f ** 2 - c) ** 2 + (4 * d * f + b * f) ** 2 ) ** 0.5) + -1 * (2 * d ** 2 + b * d + (b ** 2) / 4 - 2 * f ** 2 - c))/2), 6) ** 0.5
+
+    return ((firstTermReal + secondTermReal, firstTermImaginary + secondTermImaginary), (firstTermReal - secondTermReal, firstTermImaginary - secondTermImaginary))
 
 
 def main():
     colorValue = 0
-    endRange = 100
-    for i in range(0, endRange):
-        for j in range(0, endRange):
+    endRange = 10
+    increment = 1
+    i = 0
+    while i < endRange:
+        j = 0
+        while j < endRange:
             k = 0.0
             while k < 1:
-                if round(k, 4) == 1.0:
-                    k = 0.9
-                else:
-                    k = round(k, 4)
-                a, b, c = transformation_floor_b(i, j, k)
-                if (a.is_integer() and b.is_integer()):
-                    plt.plot(a, b, marker="o", markersize=1, markeredgecolor=str(colorValue), markerfacecolor=str(colorValue))
-                    plt.plot(a, c, marker="o", markersize=1, markeredgecolor=str(colorValue), markerfacecolor=str(colorValue))
-                k += .01
-            colorValue += 1/(endRange ** 2)
-            print(colorValue)
+                l = 0.0
+                while l < 1:
+                    a, b = transformation_floor_b(i, j, k, l)
+
+                    # if (isinstance(a[0], complex)):
+                    print((i, j, k, l))
+                    #     print(a)
+                    if colorValue > 1:
+                        colorValue = 1
+                    if (a[0].is_integer() and a[1].is_integer()):
+                        # print(f"b: {i}, c: {j}, d: {k} = {a[0]} + {a[1]}i")
+                        plt.plot(a[0], a[1], marker="o", markersize=1, markeredgecolor=str(colorValue), markerfacecolor=str(colorValue))
+                    if (b[0].is_integer() and b[1].is_integer()):
+                        # print(f"b: {i}, c: {j}, d: {k} = {b[0]} + {b[1]}i")
+                        plt.plot(a[0], a[1], marker="o", markersize=1, markeredgecolor=str(colorValue), markerfacecolor=str(colorValue))
+                    l += .1
+                k += .1
+            colorValue += 1/((endRange/.1) ** 2)
+            j += increment
+        i += increment
     print("Done")
     plt.show()
 
