cad011015809666ad67c03f88916508cadeaf6ad
vidur2 <vmod2005@gmail.com>
2022-06-30 07:56:33 -0400

    added inputs

--- floorComplexNumbers/main.py
+++ floorComplexNumbers/main.py
@@ -17,32 +17,33 @@ def transformation_floor_b(b: float, c: float, d: float, f: float):
 
 def main():
     colorValue = 0
-    endRange = 10
-    increment = 1
+    endRange = int(input("Enter the highest number you would like to test to (ie 10): "))
+    increment = float(input("Enter the amount you would like to increment by (ie 0.1): "))
     i = 0
+    colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k', 'w')
+
     while i < endRange:
         j = 0
         while j < endRange:
             k = 0.0
             while k < 1:
                 l = 0.0
+                colorIndex = 0
                 while l < 1:
                     a, b = transformation_floor_b(i, j, k, l)
 
                     # if (isinstance(a[0], complex)):
                     print((i, j, k, l))
                     #     print(a)
-                    if colorValue > 1:
-                        colorValue = 1
-                    if (a[0].is_integer() and a[1].is_integer()):
+                    if (k != 0 or l != 0):
+                        colorValue = colors[colorIndex % (len(colors) - 1)]
                         # print(f"b: {i}, c: {j}, d: {k} = {a[0]} + {a[1]}i")
                         plt.plot(a[0], a[1], marker="o", markersize=1, markeredgecolor=str(colorValue), markerfacecolor=str(colorValue))
-                    if (b[0].is_integer() and b[1].is_integer()):
                         # print(f"b: {i}, c: {j}, d: {k} = {b[0]} + {b[1]}i")
-                        plt.plot(a[0], a[1], marker="o", markersize=1, markeredgecolor=str(colorValue), markerfacecolor=str(colorValue))
+                        plt.plot(b[0], b[1], marker="o", markersize=1, markeredgecolor=str(colorValue), markerfacecolor=str(colorValue))
+                        colorIndex += 1
                     l += .1
                 k += .1
-            colorValue += 1/((endRange/.1) ** 2)
             j += increment
         i += increment
     print("Done")
