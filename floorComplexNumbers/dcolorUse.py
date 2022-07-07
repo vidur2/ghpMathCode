3ba669f628bd82548066f48c9f0bd9b0bd96ab65
vidur2 <vmod2005@gmail.com>
2022-07-06 22:01:14 -0400

    eric please run this

--- floorComplexNumbers/dcolorUse.py
+++ floorComplexNumbers/dcolorUse.py
@@ -17,38 +17,39 @@ def main():
     dc = dcolor.DColor()
 
     # Setting variables
-    endRange = 2
-    b = 1
+    endRange = 8
+    b = -endRange
 
     # List for storing roots
     roots = []
-    
-    # Finds roots
-    while b < endRange:
-        c = 1
-        while c < endRange:
-            path = rc.plot(lambda z, floor_z: z**2 + b*floor_z + c, metadata=f'z^2 + {b}[z] + {c}') # 
-            dc.plot(lambda z, floor_z: z**2 + b*floor_z + c, metadata=f'z^2 + {b}[z] + {c}')
-            roots += getRoots(path)
-            print(roots)
-            c+=.1
-        b += .1
+    try:
+        # Finds roots
+        while b < endRange:
+            c = -endRange
+            while c < endRange:
+                path = rc.plot(lambda z, floor_z: z**2 + b*floor_z + c, metadata=f'z^2 + {b}[z] + {c}') # 
+                dc.plot(lambda z, floor_z: z**2 + b*floor_z + c, metadata=f'z^2 + {b}[z] + {c}')
+                roots += getRoots(path)
+                print(roots)
+                c+=.1
+            b += .1
 
-    cv2.destroyAllWindows()
-    x = [ele.real for ele in roots]
-    y = [ele.imag for ele in roots]
+    finally:
+        cv2.destroyAllWindows()
+        x = [ele.real for ele in roots]
+        y = [ele.imag for ele in roots]
 
-    print(x)
-    print(y)
-    plt.figure().clear()
-    plt.close()
-    plt.cla()
-    plt.clf()
-    
-    plt.scatter(x, y)
-    plt.ylabel('Imaginary')
-    plt.xlabel('Real')
-    plt.savefig("./correlation_plot")
+        print(x)
+        print(y)
+        plt.figure().clear()
+        plt.close()
+        plt.cla()
+        plt.clf()
+        
+        plt.scatter(x, y)
+        plt.ylabel('Imaginary')
+        plt.xlabel('Real')
+        plt.savefig("./correlation_plot")
 
 
 if __name__ == "__main__":
