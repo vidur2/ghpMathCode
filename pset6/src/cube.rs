462813563ba5d23ea0abbfae91bffe14d0c2017d
vidur2 <vmod2005@gmail.com>
2022-07-08 13:44:35 -0400

    kirchoffs algo

--- pset6/src/cube.rs
+++ pset6/src/cube.rs
@@ -1,37 +1,44 @@
 
-#[derive(Copy, Clone, Eq, Hash, PartialEq)]
+#[derive(Copy, Clone, Eq, Hash, PartialEq, Debug)]
 pub struct Cube {
     x: i8,
     y: i8,
-    z: i8
+    z: i8,
 }
 impl Cube {
     pub fn new(x: i8, y: i8, z: i8) -> Self {
         return Self { x, y, z }
     }
 
-    pub fn step(&self) -> Vec<Self> {
+    pub fn step(&self) -> (Vec<Self>, usize) {
         let mut neighbors: Vec<Self> = Vec::new();
+        let mut conns: usize = 0;
 
         if self.x - 1 >= 0 {
             neighbors.push(Self::new(self.x - 1, self.y, self.z));
+            conns += 1
         } 
         if self.x + 1 < 5 {
             neighbors.push(Self::new(self.x + 1, self.y, self.z));
+            conns += 1
         }
         if self.y - 1 >= 0 {
             neighbors.push(Self::new(self.x, self.y - 1, self.z));
+            conns += 1
         } 
         if self.y + 1 < 5 {
             neighbors.push(Self::new(self.x, self.y + 1, self.z));
+            conns += 1
         } 
         if self.z - 1 >= 0 {
             neighbors.push(Self::new(self.x, self.y, self.z- 1));
+            conns += 1
         } 
         if self.z + 1 < 5 {
             neighbors.push(Self::new(self.x, self.y, self.z + 1));
+            conns += 1
         }
 
-        return neighbors
+        return (neighbors, conns)
     }
 }
--- pset6/src/main.rs
+++ pset6/src/main.rs
@@ -1,4 +1,5 @@
-use std::collections::HashSet;
+use core::num;
+use std::{collections::{HashSet, VecDeque}, hash::Hash};
 
 use cube::Cube;
 
@@ -7,22 +8,47 @@ mod cube;
 fn main() {
     let cube = Cube::new(2,2, 2);
 
-    let mut stack: Vec<Cube> = Vec::new();
+    let mut stack: VecDeque<Cube> = VecDeque::new();
     let mut visited: HashSet<Cube> = HashSet::new();
+    let mut future: Vec<Cube> = Vec::new();
 
-    stack.push(cube);
+    let mut matrix: [[i8; 125]; 125] = [[0i8; 125]; 125];
+
+    stack.push_back(cube);
+    future.push(cube);
 
     while stack.len() > 0 {
-        let current = stack.pop().unwrap();
+        let current = stack.pop_front().unwrap();
         visited.insert(current);
 
-        let cube_arr = current.step();
+        let (cube_arr, deg) = current.step();
+        let column = index(&current, &future);
+        matrix[column as usize][column as usize] = deg as i8;
+
         for cube in cube_arr.iter() {
             if !visited.contains(cube) {
-                stack.push(cube.clone());
+                stack.push_back(cube.clone());
             }
+
+            if index(cube, &future) == -1 {
+                future.push(cube.clone())
+            }
+
+            let row = index(&cube, &future);
+
+            matrix[column as usize][row as usize] = -1
         }
     }
 
-    println!("{}", visited.len())
+    println!("{:?}", matrix)
+}
+
+fn index(cube: &Cube, hash_set: &Vec<Cube>) -> isize {
+    for (idx, cube_check) in hash_set.iter().enumerate() {
+        if cube == cube_check {
+            return idx as isize
+        }
+    }
+
+    return -1
 }
