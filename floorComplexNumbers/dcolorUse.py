ed1ba24962c3eb92c0f884a027fa24c1d081ff21
vidur2 <vmod2005@gmail.com>
2022-07-19 11:09:48 -0400

    coeff coloring

--- floorComplexNumbers/dcolorUse.py
+++ floorComplexNumbers/dcolorUse.py
@@ -12,10 +12,15 @@ import dcolor
 import rgbcolor
 from findRoots import getRoots
 
+class Root:
+    def __init__(self, roots: list, rgba: tuple):
+        self.roots = roots
+        self.rgba = rgba
+
 def main():
 
     # Setting variables
-    endRange = 2
+    endRange = 3
 
     # Objects for plotting
     rc = rgbcolor.DColor(xmin=-endRange, xmax=endRange, ymin=-endRange, ymax=endRange)
@@ -36,9 +41,9 @@ def main():
                 while d < endRange:
                     f = -endRange
                     while f < endRange:
-                        path = rc.plot(lambda z, floor_z: z**2+ complex(b, d)*floor_z + complex(c, f), metadata=f'z^2 + ({b} + {d}i)[z] + {c} + {f}i') # 
+                        path = rc.plot(lambda z, floor_z: z*floor_z+ complex(b, d)*z + complex(c, f), metadata=f'z^2 + ({b} + {d}i)[z] + {c} + {f}i') # 
                         # dc.plot(lambda z, floor_z: z**2 + complex(b, d)*floor_z + complex(c, f), metadata=f'z^2 + ({b} + {d}i)[z] + {c} + {f}i')
-                        roots += getRoots(path, endRange)
+                        roots.append(Root(getRoots(path, endRange), ((endRange - b)/(3*endRange), (endRange - c)/(3*endRange), (endRange - d)/(3*endRange), (endRange - f)/(3*endRange))))
                         plt.close()
                         plt.figure().clear()
                         plt.cla()
@@ -53,23 +58,27 @@ def main():
         cv2.destroyAllWindows()
         x1 = []
         y1 = []
-        x2 = []
-        y2 = []
+        # x2 = []
+        # y2 = []
         thresh = .25
 
-        for ele in roots:
-            x1.append(ele.real)
-            y1.append(ele.imag)
-
-    
         plt.figure().clear()
         plt.close()
         plt.cla()
         plt.clf()
-        
-        plt.scatter(x1, y1)
+
+        for ele in roots:
+            x1 = [subele.real for subele in ele.roots]
+            y1 = [subele.imag for subele in ele.roots]
+            print(ele.rgba)
+            plt.scatter(x1, y1, color=ele.rgba)
+            x1=[]
+            y1=[]
+        # plt.scatter(x1, y1)
         plt.ylabel('Imaginary')
         plt.xlabel('Real')
+        plt.savefig("./final_correlation.png")
+        # plt.scatter(x1, y1)
         # plt.savefig("./correlation_plot")
         # regr_func_1 = logisticRegr.loga_regr(x1, y1)
         # regr_func_2 = logisticRegr.loga_regr(x2, y2)
@@ -86,9 +95,8 @@ def main():
         # plt.ylabel('Imaginary')
         # plt.xlabel('Real')
 
-        plt.savefig("./final_correlation")
-
         f = open("./data/zZfloorzFloorbz.txt", "a")
+        f.truncate(0)
 
         for line in x1:
             f.write("%s\n" % line)
